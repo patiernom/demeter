eabc0a9fadb5cea51357891eda75c6a089976c53
patiernom <marco_patierno@msn.com>
2019-01-11 02:22:16 +0100

    set up

---
+++ API/ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps : [{
+        name        : "API",
+        script      : "bootstrap.js",
+        instances   : 1,
+        exec_mode   : "cluster",
+        watch: ["./server", "./data"],
+        ignore_watch : ["node_modules"],
+        watch_options: {
+            followSymlinks: false
+        }
+    }]
+};
